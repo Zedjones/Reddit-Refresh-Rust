8f97ae8130d9dc513690feb214b6b83e383c2220
Nicholas Jones <dojoman19@gmail.com>
2018-05-16 15:05:45 -0400

    Added some more basic configuration options

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,22 @@ fn main() {
         for inner_value in value.into_array(){
             for item in inner_value.iter(){
                 let item = item.clone().into_str();
-                println!("{}: {:?}", key, item.unwrap());
+                println!("{}: {}", key, item.unwrap());
             }
         }
     }
 
+    let user_info: HashMap<String, config::Value> = 
+        settings.get_table("user_info").unwrap();
+
+    for (key, value) in user_info{
+        println!("{}: {}", key, value.into_str().unwrap());
+    }
+
+    let program_config: HashMap<String, config::Value> = 
+        settings.get_table("program_config").unwrap();
+
+    for (key, value) in program_config{
+        println!("{}: {}", key, value.into_str().unwrap());
+    }
 }
